4ae6c6a1cc0dcb78047ef00520350f6d26ceac7d
susiesolis <52222826+susiesolis@users.noreply.github.com>
2021-04-14 15:54:40 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ resource "null_resource" "env" {
 }
 
 resource "null_resource" "null0" {
-  count = 50000
+  count = 25000
   triggers = {
     value = "EXAMPLE"
   }
 }
 
 resource "null_resource" "null1" {
-  count = 50000
+  count = 25000
    triggers = {
     value = "EXAMPLE-2"
   }
