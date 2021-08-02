4d7ec5ef66432b4ab5be733bf8d61c3e008bd7b3
susiesolis <52222826+susiesolis@users.noreply.github.com>
2021-08-02 13:43:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "null_resource" "null-1" {}
+variable "count1" {}
 
-resource "null_resource" "env" {
-  provisioner "local-exec" {
-    command = "printenv"
+resource "null_resource" "null" {
+  count = var.count1
+  triggers = {
+    value = "EXAMPLE"
   }
 }
-
 resource "null_resource" "null" {
   provisioner "local-exec" {
     command = "printenv"
