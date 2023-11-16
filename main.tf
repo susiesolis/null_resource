1c2e018e017539a01065f43ec88343a14782c63a
susiesolis <52222826+susiesolis@users.noreply.github.com>
2023-11-16 09:46:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,21 @@ terraform {
 }
 
 
-
+variable "count2" {
+default = 1500
+}
 variable "count1" {
 default = 1500
 }
 
 resource "null_resource" "null0" {
   count = var.count1
+  triggers = {
+    value = "EXAMPLE"
+  }
+}
+resource "null_resource" "null01" {
+  count = var.count2
   triggers = {
     value = "EXAMPLE"
   }
